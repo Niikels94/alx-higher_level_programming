533a6ffe448945c6af208961ab35596781dbd819
niikels94 <niiamukekeli@gmail.com>
2022-08-02 12:03:36 -0700

    My python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""Defines a text file-reading function."""
+"""Read file"""
 
 
 def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout."""
+    """a function that reads a text file (UTF8)
+    and prints it to stdout
+    Args:
+        filename(any): name of the file
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
