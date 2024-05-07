ad4838821e216632fd8e8b18860502fd3bd8b58f
Bright Nii Kekeli Amu <101403206+Niikels94@users.noreply.github.com>
2024-05-07 10:27:57 -1200

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,4 +5,9 @@
     -- name VARCHAR(256) can’t be null
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT UNIQUE AUTO_INCREMENT PRIMARY KEY, state_id INT NOT NULL, CONSTRAINT FOREIGN KEY (state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
