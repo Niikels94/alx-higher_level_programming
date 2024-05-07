f28e46672812152e6aca78124ecfc0087eb5814f
Bright Nii Kekeli Amu <101403206+Niikels94@users.noreply.github.com>
2024-05-07 11:08:58 -1200

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -5,6 +5,6 @@
     -- The database name will be passed as an argument of the mysql command
 
 SELECT cities.id, cities.name, states.name
-FROM cities
-INNER JOIN states ON cities.state_id = states.id
-ORDER BY cities.id;
+FROM cities 
+JOIN states ON cities.state_id = states.id 
+ORDER BY cities.id ASC;
