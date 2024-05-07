4326aa95015993809cc056a5bae0d57e027e5914
Bright Nii Kekeli Amu <101403206+Niikels94@users.noreply.github.com>
2024-05-07 11:01:48 -1200

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -4,4 +4,7 @@
     -- You can use only one SELECT statement
     -- The database name will be passed as an argument of the mysql command
 
-SELECT DISTINCT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name 
+FROM cities
+JOIN states ON cities.state_id = states.id;
+ORDER BY cities.id ASC;
