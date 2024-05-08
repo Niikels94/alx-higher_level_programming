b91bd32001ef83f0c3965678b1dc052afc2bfad6
Bright Nii Kekeli Amu <101403206+Niikels94@users.noreply.github.com>
2024-05-07 23:40:07 -1200

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,9 +1,6 @@
 -- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 
-SELECT tv_shows.title, tv_show_genres.genre_id
-    FROM tv_genres
-    JOIN tv_show_genres
-    JOIN tv_shows
-    ON tv_shows.id = show_id
-    AND tv_genres.id = genre_id
-    ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT s.title, g.genre_id
+FROM tv_shows AS s
+JOIN tv_show_genres AS g ON s.id = g.show_id
+ORDER BY s.title, g.genre_id;
