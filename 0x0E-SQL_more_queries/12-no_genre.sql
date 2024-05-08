0e29031f161c931cb5208caeff0cfe3a65e93778
Bright Nii Kekeli Amu <101403206+Niikels94@users.noreply.github.com>
2024-05-08 01:06:32 -1200

    Update 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,8 +1,7 @@
 -- Write a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
 
-SELECT tv_shows.title, tsg.genre_id
-	FROM tv_shows
-	LEFT JOIN tv_show_genres AS tsg
-	ON tv_shows.id = tsg.show_id
-	WHERE tsg.genre_id IS NULL
-	ORDER BY tv_shows.title ASC, tsg.genre_id ASC;
+SELECT s.title, g.genre_id
+FROM tv_shows AS s
+LEFT JOIN tv_show_genres AS g ON s.id = g.show_id
+WHERE g.genre_id IS NULL;
+ORDER BY s.title, g.genre_id ASC;
