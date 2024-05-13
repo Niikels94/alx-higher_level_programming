2748aeec53f09cb0e7428452bc78c37cd895995a
Bright Nii Kekeli Amu <101403206+Niikels94@users.noreply.github.com>
2024-05-13 00:19:24 -1200

    Update 101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,13 +1,14 @@
 -- Write a script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
 
-SELECT DISTINCT title
-FROM tv_shows AS t
-LEFT JOIN tv_show_genres AS s ON s.show_id = t.id
-LEFT JOIN tv_genres AS g ON g.id = s.genre_id
-WHERE t.title NOT IN (
-    SELECT title
-    FROM tv_shows AS t
-	INNER JOIN tv_show_genres AS s ON s.show_id = t.id
-    INNER JOIN tv_genres AS g ON g.id = s.genre_id
-	WHERE g.name = "Comedy")
- ORDER BY title;
+SELECT title
+FROM tv_shows AS s
+JOIN tv_show_genres AS t ON s.id = t.show_id
+JOIN tv_genres AS g ON t.genre_id = g.id
+WHERE s.title NOT IN (
+    SELECT s.title
+    FROM tv_shows AS s
+    JOIN tv_show_genres AS t ON s.id = t.show_id
+    JOIN tv_genres AS g ON t.genre_id = g.id
+    WHERE g.name = "Comedy"
+)
+ORDER BY s.title ASC;
