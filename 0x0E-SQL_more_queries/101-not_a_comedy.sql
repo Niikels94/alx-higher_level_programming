f78f1af166487cf053eedf450dcfc1cffbb05d04
Bright Nii Kekeli Amu <101403206+Niikels94@users.noreply.github.com>
2024-05-13 00:22:39 -1200

    Update 101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,8 +2,8 @@
 
 SELECT title
 FROM tv_shows AS s
-JOIN tv_show_genres AS t ON s.id = t.show_id
-JOIN tv_genres AS g ON t.genre_id = g.id
+RIGHT JOIN tv_show_genres AS t ON s.id = t.show_id
+RIGHT JOIN tv_genres AS g ON t.genre_id = g.id
 WHERE s.title NOT IN (
     SELECT s.title
     FROM tv_shows AS s
